3ec9d54cfa2bc91801ab38cbf1164267dc9e1d49
CaveLoki <104798939+CaveLoki@users.noreply.github.com>
2022-06-23 23:19:36 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,10 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+const myInfo = () => {
+    let myName = 'Lucifer';
+    let myHeight = '5 foot 8 inches';
+    let myCountry = 'Canada';
+    let myInfo = "My name is " + myName +  " ," + " \nI' m " + myHeight + "\nI am from " + myCountry + "."
+    return myInfo;
+}
+// console.log(myDetails());
+alert(myInfo());
